a5eac890fa05d20f618fc29a0f7b747182496808
Pi-face <guzdev52@gmail.com>
2025-04-27 16:57:08 -0500

    Create function to read user inputs and arguments.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    // collects all the arguments in the console, then collects the string list into a Vector or Array.
+    if args.len() < 2 {
+        eprintln!("Usage: reed <scriptfile>");
+        std::process::exit(1);
+        // if the user did not provide more than 1 it didnt contain the file in the list.
+    }
+    let filename = &args[1];
+    println!("Filename: {}", filename);
 }
+
+ 
